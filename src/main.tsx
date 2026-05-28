2c1cd6d0fcd4d8f55131a04967868a96ee73729f
pramod55-ai <pramodapte55@gmail.com>
2026-05-28 16:32:02 +0530

    Deploy complete automated PWA installation assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-);
+  </React.StrictMode>,
+)
+
+// Triggers the Samsung/Android browser to show the "Add to Home Screen" icon option
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('PWA installation channel ready:', reg))
+      .catch(err => console.error('PWA channel offline:', err));
+  });
+}
