a5c9a18505e02bd659f8ce014d2e4f9ea8981bc7
pramod55-ai <pramodapte55@gmail.com>
2026-05-29 14:31:47 +0530

    Resolve main entrance compilation crash by removing index.css track

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Disabled broken stylesheet link that was crashing the Vite production pipeline
+// import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
-
-// Triggers the Samsung/Android browser to show the "Add to Home Screen" icon option
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('PWA installation channel ready:', reg))
-      .catch(err => console.error('PWA channel offline:', err));
-  });
-}
+  </React.StrictMode>
+);
